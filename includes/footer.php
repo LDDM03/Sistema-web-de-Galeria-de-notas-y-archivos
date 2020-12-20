8193e933b0ab51bb50ad2a49847c274ebf0a65ad
LDDM03 <62411508+LDDM03@users.noreply.github.com>
2020-12-20 01:50:09 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-size:17px;"><i class="material-icons" style="color: brown;">COPYRIGHT</i> 2020 <b >
+					Galería de notas y archivos </b> <br> Desarrollado por Luis Diaz </p>
+                </div>
+            </div>
+   </footer>
